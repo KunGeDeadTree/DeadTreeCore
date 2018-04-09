db2cdba9f71044e5b3833b53120a8d15b2f25f54
Scarlet <1996scarlet@gmail.com>
2018-04-09 17:03:44 +0800

    添加项目文件。

---
+++ DeadTree/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeadTree.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

8a75085c47c6e4fcd112c24af334bcbb61bf0ef9
ILiangk <1243086687@qq.com>
2018-04-10 14:04:08 +0800

    元件model

--- DeadTree/Models/DBClass/ComponentModel.cs
+++ DeadTree/Models/DBClass/ComponentModel.cs
@@ -4,6 +4,8 @@
     using System.ComponentModel.DataAnnotations;
     public class ComponentModel
     {
-        //sdf
+        [Key]
+        [DisplayName("元件ID")]
+        public int CId { get; set; }
     }
 }
